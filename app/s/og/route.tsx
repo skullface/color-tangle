b36a397050c24a287c8fd147fa887077e4b9d852
skullface <skullface@users.noreply.github.com>
2026-08-09 14:39:13 -0400

    generated OG image layout

--- app/s/og/route.tsx
+++ app/s/og/route.tsx
@@ -6,12 +6,22 @@ import { size } from "@/app/s/og/size";
 export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
 
+const franklinRegular = readFile(
+  join(process.cwd(), "assets/fonts/LibreFranklin-Regular.woff"),
+);
+const franklinSemiBold = readFile(
+  join(process.cwd(), "assets/fonts/LibreFranklin-SemiBold.woff"),
+);
+
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-  const score = searchParams.get("score") ?? "0";
   const correct = searchParams.get("correct") ?? "0";
 
-  const bg = await readFile(join(process.cwd(), "public/og-bg.png"));
+  const [bg, regular, semibold] = await Promise.all([
+    readFile(join(process.cwd(), "public/og-bg.png")),
+    franklinRegular,
+    franklinSemiBold,
+  ]);
   const bgSrc = `data:image/png;base64,${bg.toString("base64")}`;
 
   return new ImageResponse(
@@ -21,27 +31,62 @@ export async function GET(request: Request) {
         height: "100%",
         display: "flex",
         flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        color: "#111111",
+        alignItems: "flex-end",
+        justifyContent: "flex-start",
+        color: "000000",
         fontSize: 48,
-        fontFamily: "sans-serif",
+        fontFamily: "Libre Franklin",
         backgroundImage: `url(${bgSrc})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
     >
-      <div style={{ display: "flex", fontSize: 64, fontWeight: 700 }}>
-        Color Tangle
+      <div
+        style={{
+          display: "flex",
+          paddingTop: 120,
+          paddingRight: 120,
+          fontSize: 120,
+          maxWidth: 920,
+          letterSpacing: -2,
+          lineHeight: 1.1,
+          fontWeight: 600,
+          textAlign: "right",
+        }}
+      >
+        Can you guess {correct} out of 10 weird, rare color names?
       </div>
-      <div style={{ display: "flex", marginTop: 24 }}>{`${score} points`}</div>
-      <div style={{ display: "flex", marginTop: 12, fontSize: 36 }}>
-        {`${correct}/10 correct`}
-      </div>
-      <div style={{ display: "flex", marginTop: 32, fontSize: 28 }}>
-        Can you beat this?
+      <div
+        style={{
+          display: "flex",
+          paddingRight: 100,
+          marginTop: 24,
+          fontSize: 56,
+          lineHeight: 1.4,
+          width: 520,
+          opacity: 0.5,
+          textAlign: "right",
+        }}
+      >
+        Because I can. Try to beat me.
       </div>
     </div>,
-    { ...size },
+    {
+      ...size,
+      fonts: [
+        {
+          name: "Libre Franklin",
+          data: regular,
+          style: "normal",
+          weight: 400,
+        },
+        {
+          name: "Libre Franklin",
+          data: semibold,
+          style: "normal",
+          weight: 600,
+        },
+      ],
+    },
   );
 }
